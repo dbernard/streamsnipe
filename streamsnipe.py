5b371353d49281aa5ad07d254b58dc000689f0c6
Dylan Bernard <dylan.bernard@thehumangeo.com>
2017-05-20 10:48:37 -0700

    Nixing the streamer name lookup/matching

--- streamsnipe.py
+++ streamsnipe.py
@@ -53,8 +53,10 @@ def on_intent(intent_request, session):
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
-    if intent_name == 'GetStreamerStatusIntent':
-        return get_streamer_status_response(intent)
+    if intent_name == 'GetTopStreamersIntent':
+        return get_top_streamers_response(intent)
+    elif intent_name == 'GetFeaturedStreamersIntent':
+        return get_featured_streamers_response(intent)
     elif intent_name == 'GetStreamersByGameIntent':
         return get_streamers_by_game_response(intent)
     elif intent_name == "AMAZON.HelpIntent":
@@ -148,42 +150,6 @@ def get_welcome_response():
         card_title, speech_output, reprompt_text, should_end_session))
 
 
-def get_streamer_status_response(intent):
-    card_title = 'Streamer Status'
-    session_attributes = {}
-    reprompt_text = None
-    should_end_session = True
-
-    if 'Streamer' in intent['slots']:
-        print(intent)
-        streamer = intent['slots']['Streamer']['value']
-        status = get_streamer_status(streamer)
-
-        if len(status) == 1:
-            live = status[0]
-            speech_output = 'Yes, {} is playing {} for {} viewers.'.format(
-                                                                live['name'],
-                                                                live['game'],
-                                                                live['viewers'])
-        elif len(status) > 1:
-            names = [n['name'] for n in status]
-            speech_output = 'I found a few streamers that might match who '\
-                            'you\'re looking for. {} are streaming right '\
-                            'now.'.format(', '.join(names))
-        else:
-            speech_output = 'It doesn\'t look like anyone with the name {} is '\
-                            'streaming right now.'.format(streamer)
-
-        reprompt_text = 'Ask me if your favorite streamer is online.'
-    else:
-        speech_output = 'Sorry, I\'m not sure who you\'re looking for. Please '\
-                        'ask again.'
-        reprompt_text = speech_output
-
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
 def get_streamers_by_game_response(intent):
     card_title = 'Top Streamers by Game'
     session_attributes = {}
@@ -210,40 +176,43 @@ def get_streamers_by_game_response(intent):
         card_title, speech_output, reprompt_text, should_end_session))
 
 
+def get_featured_streamers_response(intent):
+    card_title = 'Featured Streamers'
+    session_attributes = {}
+    reprompt_text = 'Ask me who the featured streamers are right now.'
+    should_end_session = True
+
+    streamers = get_featured_streamers()
+    if streamers:
+        s = ', '.join(streamers)
+        speech_output = 'The featured streamers right now are {}'.format(s)
+    else:
+        speech_output = 'It doesn\'t look like there are any featured '\
+                        'streamers at the moment.'
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def get_top_streamers_response(intent):
+    card_title = 'Top Streamers'
+    session_attributes = {}
+    reprompt_text = 'Ask me who the top streamers are right now.'
+    should_end_session = True
+
+    streamers = get_top_streamers()
+    if streamers:
+        s = ', '.join(streamers)
+        speech_output = 'The top streamers right now are {}'.format(s)
+    else:
+        speech_output = 'It doesn\'t look like there are any top '\
+                        'streamers at the moment.'
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
 # --- TWITCH API CALLS
-
-
-def get_featured_streams():
-    pass
-
-
-def get_streamer_status(name):
-    name = name.lower().replace(' ', '')
-    headers = {'Accept': 'application/vnd.twitchtv.v3+json',
-               'Client-ID': CLIENT}
-
-    url = 'https://api.twitch.tv/kraken/search/streams?query={}'.format(name)
-    r = requests.get(url, headers=headers)
-
-    r.raise_for_status()
-    resp = json.loads(r.text)
-
-    matches = []
-
-    if not resp['streams']:
-        return matches
-
-    for stream in resp['streams']:
-        disp_name = _get_stream_name(stream)
-        game = _get_stream_game(stream)
-        viewers = _get_stream_viewers(stream)
-
-        if name in disp_name.lower().replace(' ', '').replace('_', ''):
-            streamer = {'name': disp_name, 'game': game, 'viewers': viewers}
-            matches.append(streamer)
-
-    return matches
-
 
 def get_streamers_by_game(game):
     headers = {'Accept': 'application/vnd.twitchtv.v3+json',
@@ -257,9 +226,63 @@ def get_streamers_by_game(game):
 
     top = []
     if resp['streams']:
-        streams = sorted(resp['streams'], key=lambda k: k['viewers'], reverse=True)
+        try:
+            streams = sorted(resp['streams'], key=lambda k: k['viewers'], reverse=True)
 
-        top = [k['channel']['display_name'] for k in streams[:5]]
+            top = [k['channel']['display_name'] for k in streams[:5]]
+        except AttributeError:
+            # Stream JSON has been proven unreliable. Ignore nameless/gameless
+            # results.
+            pass
+
+    return top
+
+
+def get_featured_streamers():
+    headers = {'Accept': 'application/vnd.twitchtv.v3+json',
+               'Client-ID': CLIENT}
+
+    url = 'https://api.twitch.tv/kraken/streams/featured'
+    r = requests.get(url, headers=headers)
+
+    r.raise_for_status()
+    resp = json.loads(r.text)
+
+    feat = []
+    for stream in resp['featured']:
+        try:
+            name = stream['stream']['channel']['display_name'].encode('utf-8')
+            game = stream['stream']['channel']['game'].encode('utf-8')
+            feat.append('{} playing {}'.format(name, game))
+        except AttributeError:
+            # Stream JSON has been proven unreliable. Ignore nameless/gameless
+            # results.
+            pass
+
+    return feat
+
+
+def get_top_streamers():
+    headers = {'Accept': 'application/vnd.twitchtv.v3+json',
+               'Client-ID': CLIENT}
+
+    url = 'https://api.twitch.tv/kraken/streams'
+    r = requests.get(url, headers=headers)
+
+    r.raise_for_status()
+    resp = json.loads(r.text)
+
+    top = []
+    if resp['streams']:
+        try:
+            streams = sorted(resp['streams'], key=lambda k: k['viewers'], reverse=True)
+
+            top = ['{} playing {}'.format(k['channel']['display_name'].encode('utf-8'),
+                                      k['channel']['game'].encode('utf-8')) for k in streams[:5]]
+        except AttributeError:
+            # Stream JSON has been proven unreliable. Ignore nameless/gameless
+            # results.
+            pass
 
     return top
 
@@ -286,6 +309,8 @@ def _get_stream_game(stream):
 
 
 if __name__ == '__main__':
-    print(get_streamer_status('Tim the tat man'))
+    #print(get_streamers_by_game('overwatch'))
 
-    print(get_streamers_by_game('overwatch'))
+    #print(get_top_streamers())
+
+    print(get_featured_streamers())
